d3937691e45e06b68cde0bdea1036db4ef4d0713
Cibirsk <stvl@gmx.com>
2021-04-04 11:48:27 +0200

    none

---
+++ filter_out_the_geese.js
@@ -0,0 +1,10 @@
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let outGeese = birds;
+  for(let i=0;i<outGeese.length;i++){
+    
+  }
+  // return an array containing all of the strings in the input array except those that match strings in geese
+};
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
